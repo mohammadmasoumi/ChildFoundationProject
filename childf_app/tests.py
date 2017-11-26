151ee1419f1a31cb3d68e191fd0d9cdd827f685e
mohammadmasoumi <mohammad.masoomy74@gmail.com>
2017-11-26 11:17:15 +0330

    setup travic for continuous integration test

--- childf_app/tests.py
+++ childf_app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class PaymentTestCase(TestCase):
+    def setUp(self):
+        print('setup for test ')
+
+    def test_payment(self):
+        print('Hello')
