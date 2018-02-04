5dc4b9846a92c3a497139306d979c1cfb3d77143
DeathAngel <mohammad.masoomy74@gmail.com>
2018-02-04 14:20:33 +0330

    ccreating UI for madadkar but need tobe completed

--- childf_app/models.py
+++ childf_app/models.py
@@ -1,4 +1,27 @@
+import random
 from django.db import models
 
 # Create your models here.
+
+
+class HasUserMixin:
+    user_relate_name = None
+    user = models.OneToOneField('auth.User', related_name=user_relate_name)
+
+
+class MadadJou(HasUserMixin, models.Model):
+    user_relate_name = 'madadjou'
+
+
+
+class MadadKar(HasUserMixin, models.Model):
+    user_relate_name = 'madadkar'
+
+
+def random_string():
+    return str(random.randint(10000, 99999))
+
+class HamYar(HasUserMixin, models.Model):
+    user_relate_name = 'hamyar'
+    activation_code = models.CharField(max_length=6, default=random_string)
 
